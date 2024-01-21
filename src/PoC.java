0a2bc93660f38b43bf3f62241dd6d2adff23989f
Willi Lazarov <lazarov.w@gmail.com>
2024-01-21 10:52:33 +0100

    Extend PoC

--- src/PoC.java
+++ src/PoC.java
@@ -1,10 +1,7 @@
 import com.herumi.mcl.Mcl;
 import cz.vut.feec.lazarov.smartgrid.*;
 
-
 public class PoC {
-    private static final int delay = 1000; // in millis
-
     static {
         String osName = System.getProperty("os.name").toLowerCase();
 
@@ -22,15 +19,18 @@ public class PoC {
     public static void main(String[] args) throws Exception {
         Mcl.SystemInit(Mcl.BN254);
 
-        Trader t1 = new Trader("E.ON", 666);
-        Trader t2 = new Trader("FONERGY", 777);
-        SmartGrid sg1 = new SmartGrid("EG.D", 333);
-        SmartGrid sg2 = new SmartGrid("ČEZ Distribuce", 555);
+        Trader t1 = new Trader("E.ON", 100);
+        Trader t2 = new Trader("FONERGY", 101);
+        SmartGrid sg1 = new SmartGrid("EG.D", 200);
+        SmartGrid sg2 = new SmartGrid("ČEZ Distribuce", 201);
+        SmartGrid sg3 = new SmartGrid("PREdistribuce", 202);
 
         sg1.addTrader(t1.getPublicKey(), t1.getPort());
         sg1.addTrader(t2.getPublicKey(), t2.getPort());
         sg2.addTrader(t1.getPublicKey(), t1.getPort());
         sg2.addTrader(t2.getPublicKey(), t2.getPort());
+        sg3.addTrader(t1.getPublicKey(), t1.getPort());
+        sg3.addTrader(t2.getPublicKey(), t2.getPort());
 
         HomeEnergyManagementSystem hems1 = new HomeEnergyManagementSystem("Preslova 301/29, 602 00 Brno");
         hems1.agreeTariffWithTrader(t1.getPort());
@@ -39,24 +39,29 @@ public class PoC {
         sm1.signAndSendConsumption(sg1.getPort());
 
         HomeEnergyManagementSystem hems2 = new HomeEnergyManagementSystem("Sochorova 3214/44, 616 00 Brno");
-        hems2.agreeTariffWithTrader(t1.getPort());
+        hems2.agreeTariffWithTrader(t2.getPort());
         SmartMeter sm2 = new SmartMeter(1525, "Itron");
         hems2.connectSmartMeter(sm2, hems2.getClient());
         sm2.signAndSendConsumption(sg1.getPort());
 
         HomeEnergyManagementSystem hems3 = new HomeEnergyManagementSystem("Raisova 889/3, 460 01 Liberec");
-        hems3.agreeTariffWithTrader(t2.getPort());
+        hems3.agreeTariffWithTrader(t1.getPort());
         SmartMeter sm3 = new SmartMeter(3000, "Landis+Gyr");
         hems3.connectSmartMeter(sm3, hems3.getClient());
         sm3.signAndSendConsumption(sg2.getPort());
 
-        System.out.println("Trader" + t1.getName() + " clients:");
+        HomeEnergyManagementSystem hems4 = new HomeEnergyManagementSystem("Boleslavova 1529/28, 140 00 Praha");
+        hems4.agreeTariffWithTrader(t2.getPort());
+        SmartMeter sm4 = new SmartMeter(3001, "Landis+Gyr");
+        hems4.connectSmartMeter(sm4, hems4.getClient());
+        sm4.signAndSendConsumption(sg3.getPort());
+
         t1.showClientsConsumption();
-        System.out.println("Trader" + t2.getName() + " clients:");
         t2.showClientsConsumption();
 
         sg1.close();
         sg2.close();
+        sg3.close();
         t1.close();
         t2.close();
     }
